fe591e10cb52f059e9969403c6240aa199b41ab8
Manuel Palmero <manueligna06@gmail.com>
2025-01-28 02:36:48 -0300

    beep test

--- comandos/beep.py
+++ comandos/beep.py
@@ -3,13 +3,18 @@ from telegram.ext import *
 import os
 
 async def beep(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    frecuencia = context.args[0]
-    tiempo = context.args[1]
-    if not tiempo:
-        await context.bot.send_message(update.effective_chat.id, f"No se especifico el tiempo")
-    elif not frecuencia:
-        await context.bot.send_message(update.effective_chat.id, f"No se especifico la frecuencia")
-    else:
+    print("context.args: ",context.args)
+    print("len context.args: ",len(context.args))
+    try:
+        frecuencia = context.args[0]
+    except IndexError:
+        await context.bot.send_message(update.effective_chat.id, f"No se especificó la frecuencia")
+        print(f"No se especificó la frecuencia")
+    try:
+        tiempo = context.args[1]
+    except IndexError:
+        await context.bot.send_message(update.effective_chat.id, f"No se especificó el tiempo")
+
         os.system(f'beep -f {frecuencia} -l {tiempo}')
         await context.bot.send_message(update.effective_chat.id, f"Emitiendo un pitido de {frecuencia} Hz durante {tiempo/1000} segundos")
 
